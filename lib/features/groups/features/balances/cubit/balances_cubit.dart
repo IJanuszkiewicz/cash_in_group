fc96e05f1761896fcf3a1d23d6a027499b9ad409
Igor Januszkiewicz <igorjanuszkiewicz997@gmail.com>
2025-01-15 21:34:51 +0100

    Start work on balances

--- lib/features/groups/features/balances/cubit/balances_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-sealed class BalancesState {}
-
-class BalancesCubit extends Cubit<BalancesState> {
-  BalancesCubit(super.initialState);
-}
---
+++ lib/features/groups/features/group/presentation/widgets/balances_screen.dart
@@ -0,0 +1,13 @@
+import 'package:cash_in_group/features/groups/features/group/cubits/group_state.dart';
+import 'package:flutter/material.dart';
+
+class BalancesScreen extends StatelessWidget {
+  const BalancesScreen({super.key, required this.loadedState});
+
+  final GroupLoaded loadedState;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
